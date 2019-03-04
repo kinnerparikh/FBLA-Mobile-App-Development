4c7300744e246fc5f071b7f03cf1747155a8b0e5
Kinner Parikh <kinnerpa@gmail.com>
2019-03-03 22:24:48 -0800

    Reset Button on End Scene

--- FBLA Mobile App Development/Assets/Scripts/EndController.cs
+++ FBLA Mobile App Development/Assets/Scripts/EndController.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EndController : MonoBehaviour
 {
     [SerializeField]
     private GameObject scoreText;
 
+    public Button m_firstButton;
 
     void Start()
     {
         scoreText.GetComponent<TextMeshProUGUI>().text = "Score: " + GameController.numCorrect;
+        m_firstButton.onClick.AddListener(TaskOnClick);
     }
 
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Topic");
+        GameController.numCorrect = 0;
+    }
 }
