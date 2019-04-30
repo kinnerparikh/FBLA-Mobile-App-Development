6d3997b1a30d94d8df160f64955ad45b7c0056c2
Unknown <kinnerpa@gmail.com>
2019-04-29 17:08:20 -0700

    Leaderboards Created

--- FBLA Mobile App Development/Assets/Scripts/LeaderBoardsController.cs
+++ FBLA Mobile App Development/Assets/Scripts/LeaderBoardsController.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text;
 
 public class LeaderBoardsController : MonoBehaviour
 {
     //High score text 
     public TMP_Text leaderboardText;
+    const string webUrl = "http://dreamlo.com/lb/fSJ0kZGVv0mxWHfmsF6fqASwUXyLPUE0KRhYaWOUzJ7Q";
+    public Highscore[] highscoreList;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Update leaderboard with high scores from HighScore class
+        StartCoroutine(DownloadHighScoreFromDatabase());
+
+        /*// Update leaderboard with high scores from HighScore class
         List<Score> scores = HighScores.GetScores();
         Debug.Log(scores.Count);
         string scoresString = "";
@@ -20,12 +25,76 @@ public class LeaderBoardsController : MonoBehaviour
             scoresString += scores[i].Name + " - " + scores[i].Val + "\n";
         }
         Debug.Log(scoresString);
-        leaderboardText.text = scoresString;
+        leaderboardText.text = scoresString;*/
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator DownloadHighScoreFromDatabase()
+    {
+        WWW www = new WWW(webUrl + "/pipe/");
+        yield return www;
+
+        Debug.Log(www.text);
+        if (string.IsNullOrEmpty(www.error))
+            FormatHighScores(www.text);
+
+        
+
+        string scoreString = "";
+        for (int i = 0; i < Mathf.Min(highscoreList.Length, 10); i++)
+        {
+            string str = highscoreList[i].getUsername() + " - " + highscoreList[i].getScore() + "\n";
+            StringBuilder newStringB = new StringBuilder(str);
+            newStringB.Replace("+", " ");
+
+            scoreString += newStringB.ToString();
+        }
+
+        Debug.Log(scoreString);
+        leaderboardText.text = scoreString;
+
+    }
+
+    void FormatHighScores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoreList = new Highscore[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split('|');
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoreList[i] = new Highscore(username, score);
+
+            Debug.Log(highscoreList[i].username + ": " + highscoreList[i].score);
+        }
+    }
+}
+
+public struct Highscore
+{
+    public string username;
+    public int score;
+
+    public Highscore(string _username, int _score)
+    {
+        username = _username;
+        score = _score;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public string getUsername()
+    {
+        return username;
     }
 }
